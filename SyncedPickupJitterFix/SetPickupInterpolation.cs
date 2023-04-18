4de059a6ac881111b447c5c0066cf6afd225d996
Superbstingray <74171114+Superbstingray@users.noreply.github.com>
2023-04-18 20:22:33 +1200

    Update SetPickupInterpolation.cs

--- SyncedPickupJitterFix/SetPickupInterpolation.cs
+++ SyncedPickupJitterFix/SetPickupInterpolation.cs
@@ -1,4 +1,4 @@
-﻿
+
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -9,30 +9,69 @@ namespace Superbstingray
 	[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
 	public class SetPickupInterpolation : UdonSharp.UdonSharpBehaviour
 	{
+	
+	public bool setKinematic;
+	bool kinematicRight, kinematicLeft, nullRight, nullLeft;
 
 	VRCPlayerApi localPlayer;
 	VRC_Pickup pickupRight, pickupLeft;
+	Rigidbody rigidRight, rigidLeft;
 
 		void Start()
 		{
 			localPlayer = Networking.LocalPlayer;
 		}
 
-		public void FixedUpdate()
+		void FixedUpdate()
 		{
 			if (!VRC.SDKBase.Utilities.IsValid(localPlayer)) { return; }
 
 			pickupLeft = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Left);
-			pickupRight = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Right);
 
 			if (VRC.SDKBase.Utilities.IsValid(pickupLeft))
 			{
-				pickupLeft.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+				rigidLeft = pickupLeft.GetComponent<Rigidbody>();
+				rigidLeft.interpolation = RigidbodyInterpolation.None;
+				if(nullLeft)
+				{
+					kinematicLeft = rigidLeft.isKinematic;
+				}
+
+				if(setKinematic)
+				{
+					rigidLeft.isKinematic = true;
+					nullLeft = false;
+				}
 			}
+			else if (VRC.SDKBase.Utilities.IsValid(rigidLeft))
+			{
+				rigidLeft.isKinematic = kinematicLeft;
+				rigidLeft = null;
+				nullLeft = true;
+			}
+
+			pickupRight = localPlayer.GetPickupInHand(VRC_Pickup.PickupHand.Right);
 
 			if (VRC.SDKBase.Utilities.IsValid(pickupRight))
 			{
-				pickupRight.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+				rigidRight = pickupRight.GetComponent<Rigidbody>();
+				rigidRight.interpolation = RigidbodyInterpolation.None;
+				if(nullRight)
+				{
+					kinematicRight = rigidRight.isKinematic;
+				}
+
+				if(setKinematic)
+				{
+					rigidRight.isKinematic = true;
+					nullRight = false;
+				}
+			}
+			else if (VRC.SDKBase.Utilities.IsValid(rigidRight))
+			{
+				rigidRight.isKinematic = kinematicRight;
+				rigidRight = null;
+				nullRight = true;
 			}
 		}
 	}
